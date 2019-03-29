71dbc21c5974b7e06ed9e415fe77a36a1b70dee0
carly <carly@noukod.com>
2019-03-29 17:56:35 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,20 +1,20 @@
 // Code ClickityClick Component Here
 import React from 'react';
- 
+
 class ClickityClick extends React.Component {
   constructor() {
     super();
- 
+
     // Define the initial state:
     this.state = {
       hasBeenClicked: false
     };
   }
- 
+
   handleClick = () => {
     // Update our state here...
   };
- 
+
   render() {
     return (
       <div>
@@ -24,13 +24,13 @@ class ClickityClick extends React.Component {
     );
   }
 }
- 
+
 export default ClickityClick;
- 
+
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
- 
+
 import ClickityClick from './components/ClickityClick';
- 
+
 ReactDOM.render(<ClickityClick />, document.getElementById('root'));
